81101325ee28f3ffebc9e49f7658a24b65bdddb4
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-03-03 15:14:58 +0200

    Implemented the functionality of the addition table using HTML, CSS, and JavaScript.

--- Addition Square/script.js
+++ Addition Square/script.js
@@ -1,35 +1,97 @@
 var hiddenNums = document.querySelectorAll('.invisible');
 var visibleNums = document.querySelectorAll('.visible');
-var testV = document.getElementById('test');
+var skyBlue = document.querySelectorAll('.skyblue');
+var turtle = document.querySelectorAll('.turquoise');
+var fruit = document.querySelectorAll('.tomato');
 
+// The function below hides the numbers when the page loads
 window.addEventListener("load", function () {
     hiddenNums.forEach(number => {
-        number.textContent = ''
+        number.innerText = ''
+    });
+
+// The 3 functions below remove the background color when the page loads
+    skyBlue.forEach(box => {
+        box.classList.remove("skyblue")
+    });
+    
+    turtle.forEach(box => {
+        box.classList.remove("turquoise")
+    });
+    
+    fruit.forEach(box => {
+        box.classList.remove("tomato")
     });
 })
 
+
+// The below function handles the visible numbers
 visibleNums.forEach(number => {
-    number.addEventListener("click", function (evt) {
+number.addEventListener("click", function (evt) {
         const eventTarget = evt.target;
-        if(eventTarget.textContent.length > 0){
-            eventTarget.textContent = ''
-        } else {
-            eventTarget.textContentw
+        if(eventTarget.dataset.show == "true"){
+            eventTarget.innerText = '';
+            eventTarget.dataset.show = 'false';
+        } else if(eventTarget.dataset.show == "false"){
+            eventTarget.dataset.show = 'true';
+            eventTarget.innerText = eventTarget.dataset.value;
         }
     })
 });
 
-// In progress
+// The below function handles the hidden numbers
 hiddenNums.forEach(number => {
     number.addEventListener("click", function (evt) {
         const eventTarget = evt.target;
-        if(eventTarget.textContent == ''){
-            eventTarget.textContent.initial
+        if(eventTarget.dataset.show == "false"){
+            eventTarget.innerText = eventTarget.dataset.value;
+            eventTarget.dataset.show = true;
         } else {
             eventTarget.textContent = '';
+            eventTarget.dataset.show = false;
         }
     })
 })
+
+// The three functions below toggle the background color of each box
+skyBlue.forEach(box => {
+    box.addEventListener("click", function (e) {
+        const eventTarget = e.target;
+        if(eventTarget.dataset.sky == "off"){
+            box.classList.add('skyblue')
+            eventTarget.dataset.sky = "on"
+        } else {
+            box.classList.remove('skyblue')
+            eventTarget.dataset.sky = "off"
+        }
+    })
+});
+
+turtle.forEach(box => {
+    box.addEventListener("click", function (e) {
+        const eventTarget = e.target;
+        if(eventTarget.dataset.turtle == "off"){
+            box.classList.add('turquoise')
+            eventTarget.dataset.turtle = "on"
+        } else {
+            box.classList.remove('turquoise')
+            eventTarget.dataset.turtle = "off"
+        }
+    })
+});
+
+fruit.forEach(box => {
+    box.addEventListener("click", function (e) {
+        const eventTarget = e.target;
+        if(eventTarget.dataset.fruit == "off"){
+            box.classList.add('tomato')
+            eventTarget.dataset.fruit = "on"
+        } else {
+            box.classList.remove('tomato')
+            eventTarget.dataset.fruit = "off"
+        }
+    })
+});
 
 
 
