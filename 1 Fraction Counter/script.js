806d9a70bb8f6fe18cbcfd9a18f22ef0feff7d27
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-03-17 08:44:11 +0200

    Added CSS and JS files for simplicity

--- 1 Fraction Counter/script.js
+++ 1 Fraction Counter/script.js
@@ -1,10 +1,98 @@
 var stepSize = document.getElementById('stepSize');
 var showCover = document.getElementById('showCover');
+var secondHand = document.querySelector('.sec_hand');
+var face = document.querySelector(".clock_face");
+var hourHand = document.querySelector(".hour_hand");
+var minHand = document.querySelector(".min_hand");
+var bg = document.querySelector("body");
+var center = document.querySelector(".center");
+var rim = document.querySelector(".clock_face");
 
+// Hides the step size container
 stepSize.addEventListener('click', function(){
   stepSize.classList.add('hide');
 });
 
+// Reveals the step size container
 showCover.addEventListener('click', function(){
   stepSize.classList.remove('hide');
 });
+
+// Seconds hand
+function second(){
+  var now = new Date().getSeconds();
+
+  const click = ((now/60) * 360 + 90);
+  secondHand.style.transform = "rotate(" + click + "deg)";
+  console.log(now);
+  console.log(click);
+  var r = click - 344;
+  var g = click - 222;
+  var b = click - 4;
+  if(r < 0, g < 0, b < 0){
+    var r = r + 155;
+    var g = g + 255;
+    var b = b + 355;
+    console.log(r, g, b);
+  } else if(r > 255, g > 255, b > 255){
+    var r = r + 220;
+    var g = g - 33;
+    var b = b - 323;
+    console.log(r, g, b);
+  }
+
+  face.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+  if (now % 2 === 0){
+    center.style.border = "20px solid white";
+    center.style.backgroundColor = "white";
+    rim.style.border = "30px solid black";
+    secondHand.style.backgroundColor = "black";
+  } else if(now % 2 ==!0){
+    center.style.border = "20px solid black";
+    center.styke.backgroundColor = "black";
+    rim.style.backgroundColor = "30px solid white";
+    secondHand.style.backgroundColor = "white";
+  }
+}
+
+// Hour hand
+function hour(){
+  const now = new Date();
+  const hours = now.getHours();
+  const click = ((hours/12) * 360 + 90);
+  hourHand.style.transform = `rotate(${click}deg)`;
+  console.log(hours);
+  console.log(hours);
+}
+
+// Minute hand
+function minute(){
+  const now = new Date();
+  const minutes = now.getMinutes();
+  console.log(minutes);
+  const click = ((minute/60) * 360 + 90);
+  minHand.style.transform = `rotate(${click}deg)`;
+  var ran = Math.round(Math.random());
+  console.log(click);
+  if(minute % 2===0){
+    bg.style.backgroundColor = `rgb(${ran * 2},${minutes * 8},${minutes * 7})`;
+    // test
+    center.style.border = "20px solid white";
+    center.style.backgroundColor = "white";
+    rim.style.border = "30px solid black";
+    // test
+  } else if(minute % 2==!0){
+    bg.style.backgroundColor = `rgb(${ran * 4},${minutes * 4},${minutes * 6})`;
+    // test
+    center.style.border = "20px solid black";
+    center.style.backgroundColor = "black";
+    rim.style.border = "30px solid white";
+    // test
+  }
+}
+
+setInterval(hour);
+setInterval(second);
+setInterval(minute);
+
---
+++ How strong is the centripetal force (bespoke)/script.js
@@ -0,0 +1,143 @@
+//Speed__________________________________________________
+var speedIndex = 3;
+var speedKnobPos = ['0','85px','170px','255px','340px'];
+var sknobPosition = document.getElementById('speedknob');
+var truckSpeed = document.getElementById('rotateMe');
+var rotationSpeeds = ['60s','42s','28s','18s','10s'];
+var speedValue = [3];
+
+function checkSpeed(){
+  sknobPosition.style.left = speedKnobPos[speedIndex - 1];
+  truckSpeed.style.animationDuration = rotationSpeeds[speedIndex - 1];
+  checkArrowScale();
+}
+
+function addSpeed(){
+  if (speedIndex >= 1){
+    if (speedIndex <5){
+      speedIndex = speedIndex + 1;
+      speedValue.push(speedIndex);
+    }
+  }
+  checkSpeed()
+}
+
+function lessSpeed(){
+  if (speedIndex <=5){
+    if (speedIndex > 1){
+      speedIndex = speedIndex - 1;
+      speedValue.push(speedIndex);
+    }
+  }
+  checkSpeed()
+}
+
+// Mass__________________________________________________
+var massIndex = 3;
+var massKnobPos = ['0','85px','170px','255px','340px'];
+var mknobPosition = document.getElementById('massknob');
+var mytruck = document.getElementById('mytruck');
+var truckSource = ['truckempty','truck25','truck50','truck75','truckfull'];
+var massValue = [3];
+
+
+function checkMass() {
+  mknobPosition.style.left = massKnobPos[massIndex - 1];
+  var massGraphic = 'images/' + truckSource[massIndex - 1] + '.png';
+  mytruck.src = massGraphic;
+  checkArrowScale();
+}
+
+function addMass(){
+  if (massIndex >= 1){
+    if (massIndex <5){
+      massIndex = massIndex + 1;
+      massValue.push(massIndex);
+    }
+  }
+  checkMass();
+}
+
+function lessMass(){
+  if (massIndex <=5){
+    if (massIndex > 1){
+      massIndex = massIndex - 1;
+      massValue.push(massIndex);
+    }
+  }
+  checkMass();
+}
+
+// Radius______________________________________________
+var radiusIndex = 3;
+var radiusValue = [3];
+var radiusPos = ['0','25px','50px','75px','100px'];
+var radiusKnobPos = ['340px','255px','170px','85px','0'];
+var truckPosition = document.getElementById('truck');
+var rknobPosition = document.getElementById('radiusknob');
+var myArrow = document.getElementById('myarrow');
+var arrowPos = ['-20px','5px','30px','55px','80px'];
+
+
+function checkRadius(){
+  truckPosition.style.left = radiusPos[radiusIndex - 1];
+  rknobPosition.style.left = radiusKnobPos[radiusIndex - 1];
+  myArrow.style.left = arrowPos[radiusIndex - 1];
+  checkArrowScale();
+}
+
+function lessRadius(){
+  if (radiusIndex >= 1){
+    if (radiusIndex < 5){
+      radiusIndex = radiusIndex + 1;
+      radiusValue.push(radiusIndex);
+    }
+  }
+  checkRadius();
+}
+
+function addRadius(){
+  if (radiusIndex <= 5){
+    if (radiusIndex > 1){
+      radiusIndex = radiusIndex - 1;
+      radiusValue.push(radiusIndex);
+    }
+  }
+  checkRadius();
+}
+
+// Arrow Scale_________________________________________
+var endGame = document.getElementById('endGame');
+function checkArrowScale(){
+  var arrowScale = (massValue[massValue.length-1]*(speedValue[speedValue.length-1]*1))/(6-radiusValue[radiusValue.length-1]);
+  var growth = (((arrowScale/62.5)*100)+10).toString() + '%';
+  console.log(growth);
+  if (arrowScale == 25){
+    truckSpeed.classList.remove('rotate');
+    endGame.style.display = 'block';
+  }
+  myArrow.style.width = growth;
+}
+
+
+// General_____________________________________________
+function start() {
+  var y = document.getElementById("startPanel");
+  var yy = document.getElementById("startButton");
+  y.style.display = 'none';
+  yy.style.display = 'none';
+}
+
+function hideHelp() {
+  var x = document.getElementById("help-text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function resetAll() {
+  window.location.reload();
+  return false;
+}
