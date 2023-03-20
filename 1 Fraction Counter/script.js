9a395fb4b9333e58d7fe4c2caa789771d6baa382
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-03-20 14:43:17 +0200

    Editing the 3 documents

--- 1 Fraction Counter/script.js
+++ 1 Fraction Counter/script.js
@@ -13,6 +13,8 @@ var stepSizeNumerator = document.getElementById("numeratorI"); // Displayed valu
 var addDenominator = document.getElementById("addD"); // Addition icon for numerator
 var minusDenominator = document.getElementById("minusD"); // Subtraction icon for numerator
 var stepSizeDenominator = document.getElementById("denominatorI"); // Displayed value of denominator
+var clockWise = document.getElementById("clockwise"); // Clockwise button
+var antiClockWise = document.getElementById("anti-clockwise"); // Anti-clockwise button
 
 // Hides the step size container
 stepSize.addEventListener('click', function () {
@@ -24,9 +26,10 @@ showCover.addEventListener('click', function () {
   stepSize.classList.remove('hide');
 });
 
+var number = 1;
+var number2 = 2;
+
 function displayDigits() {
-  var number = 1;
-  var number2 = 2;
 
   // Increases the value of the numerator step size
   addNumerator.addEventListener("click", function () {
@@ -69,11 +72,15 @@ function displayDigits() {
     } else if (number2 > 0) {
       number2--;
       stepSizeDenominator.innerText = number2;
-    } 
+    }
   });
 }
 
 displayDigits();
+
+function rotateArm() {
+  console.log(number);
+}
 
 // Seconds hand
 function second() {
@@ -81,77 +88,15 @@ function second() {
 
   const click = ((now / 60) * 360 + 90);
   secondHand.style.transform = "rotate(" + click + "deg)";
-  // console.log(now);
+  console.log(now);
   // console.log(click);
-  var r = click - 344;
-  var g = click - 222;
-  var b = click - 4;
-  if (r < 0, g < 0, b < 0) {
-    var r = r + 155;
-    var g = g + 255;
-    var b = b + 355;
-    // console.log(r, g, b);
-  } else if (r > 255, g > 255, b > 255) {
-    var r = r + 220;
-    var g = g - 33;
-    var b = b - 323;
-    // console.log(r, g, b);
-  }
-
-  face.style.backgroundColor = `rgb(${r},${g},${b})`;
-
-  if (now % 2 === 0) {
-    center.style.border = "10px solid white";
-    center.style.backgroundColor = "white";
-    rim.style.border = "30px solid black";
-    // secondHand.style.backgroundColor = "black";
-    secondHand.style.fill = "black";
-  } else if (now % 2 == !0) {
-    center.style.border = "10px solid black";
-    center.style.backgroundColor = "black";
-    rim.style.backgroundColor = "30px solid white";
-    // secondHand.style.backgroundColor = "white";
-    secondHand.style.fill = "white";
-  }
 }
-
-// Hour hand
-// function hour() {
-//   const now = new Date();
-//   const hours = now.getHours();
-//   const click = ((hours / 12) * 360 + 90);
-//   hourHand.style.transform = `rotate(${click}deg)`;
-//   // console.log(hours);
-//   // console.log(hours);
-// }
-
-// Minute hand
-// function minute() {
-//   const now = new Date();
-//   const minutes = now.getMinutes();
-//   // console.log(minutes);
-//   const click = ((minute / 60) * 360 + 90);
-//   minHand.style.transform = `rotate(${click}deg)`;
-//   var ran = Math.round(Math.random());
-//   // console.log(click);
-//   if (minute % 2 === 0) {
-//     bg.style.backgroundColor = `rgb(${ran * 2},${minutes * 8},${minutes * 7})`;
-//     // test
-//     center.style.border = "20px solid white";
-//     center.style.backgroundColor = "white";
-//     rim.style.border = "30px solid black";
-//     // test
-//   } else if (minute % 2 == !0) {
-//     bg.style.backgroundColor = `rgb(${ran * 4},${minutes * 4},${minutes * 6})`;
-//     // test
-//     center.style.border = "20px solid black";
-//     center.style.backgroundColor = "black";
-//     rim.style.border = "30px solid white";
-//     // test
-//   }
-// }
 
 // setInterval(hour);
 // setInterval(second);
 // setInterval(minute);
+
+// setInterval(() => {
+//   second();
+// }, 1000);
 
