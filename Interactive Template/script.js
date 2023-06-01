49d651bb2d1dbc070c5670dc7db3d68987ac415c
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-01 09:20:43 +0200

    Refactored helper functions

--- Interactive Template/script.js
+++ Interactive Template/script.js
@@ -2,22 +2,33 @@
 var checkButton = document.getElementById('checkButton');
 var solveButton = document.getElementById('solveButton');
 var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var helpButton = document.getElementById('helpButton');
 var startPanel = document.getElementById('startPanel');
 
-function hideHelp() {
+helpButton.addEventListener("mouseover", function () {
     var x = document.getElementById("help-text");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-}
+})
 
-function resetAll() {
+helpButton.addEventListener("mouseout", function () {
+    var x = document.getElementById("help-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+resetButton.addEventListener("click", function () {
     window.location.reload();
     // window.location.href = "";
     return false;
-}
+})
 
 function check() {
     console.log('check initiated');
@@ -27,8 +38,8 @@ function solve() {
     console.log('solve initiated');
 }
 
-function start() {
+startButton.addEventListener("click", function () {
     startPanel.style.display = 'none';
     startButton.style.display = 'none';
-}
+})
 // Bespoke functions_____________________________________
--- Reading Journals/script.js
+++ Reading Journals/script.js
@@ -2,63 +2,68 @@
 var checkButton = document.getElementById('checkButton');
 var solveButton = document.getElementById('solveButton');
 var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var helpButton = document.getElementById('helpButton');
 var startPanel = document.getElementById('startPanel');
 
-function hideHelp() {
-    var x = document.getElementById("help-text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+helpButton.addEventListener("mouseover", function () {
+  var x = document.getElementById("help-text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+})
 
-function resetAll() {
-    window.location.reload();
-    // window.location.href = "";
-    return false;
-}
+helpButton.addEventListener("mouseout", function () {
+  var x = document.getElementById("help-text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+})
+
+resetButton.addEventListener("click", function () {
+  window.location.reload();
+  return false;
+})
 
 function check() {
-    console.log('check initiated');
+  console.log('check initiated');
 }
 
 function solve() {
-    console.log('solve initiated');
+  console.log('solve initiated');
 }
 
-function start() {
-    startPanel.style.display = 'none';
-    startButton.style.display = 'none';
-}
+startButton.addEventListener("click", function () {
+  startPanel.style.display = 'none';
+  startButton.style.display = 'none';
+})
+
 // Bespoke functions_____________________________________
 var pages = document.getElementsByClassName('page');
-for(var i = 0; i < pages.length; i++)
-  {
-    var page = pages[i];
-    if (i % 2 === 0)
-      {
-        page.style.zIndex = (pages.length - i);
-      }
+for (var i = 0; i < pages.length; i++) {
+  var page = pages[i];
+  if (i % 2 === 0) {
+    page.style.zIndex = (pages.length - i);
   }
+}
 
-document.addEventListener('DOMContentLoaded', function(){
-  for(var i = 0; i < pages.length; i++)
-    {
-      //Or var page = pages[i];
-      pages[i].pageNum = i + 1;
-      pages[i].onclick=function()
-        {
-          if (this.pageNum % 2 === 0)
-            {
-              this.classList.remove('flipped');
-              this.previousElementSibling.classList.remove('flipped');
-            }
-          else
-            {
-              this.classList.add('flipped');
-              this.nextElementSibling.classList.add('flipped');
-            }
-         }
+document.addEventListener('DOMContentLoaded', function () {
+  for (var i = 0; i < pages.length; i++) {
+    //Or var page = pages[i];
+    pages[i].pageNum = i + 1;
+    pages[i].onclick = function () {
+      if (this.pageNum % 2 === 0) {
+        this.classList.remove('flipped');
+        this.previousElementSibling.classList.remove('flipped');
       }
+      else {
+        this.classList.add('flipped');
+        this.nextElementSibling.classList.add('flipped');
+      }
+    }
+  }
 })
