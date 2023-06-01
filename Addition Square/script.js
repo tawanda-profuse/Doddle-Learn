e4c8296ec556d39372402d89d27b5341070a307a
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-01 11:03:25 +0200

    Added the new skin to the Addition Square interactive

--- Addition Square/script.js
+++ Addition Square/script.js
@@ -1,3 +1,44 @@
+// General_____________________________________________
+var checkButton = document.getElementById('checkButton');
+var solveButton = document.getElementById('solveButton');
+var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var helpButton = document.getElementById('helpButton');
+var startPanel = document.getElementById('startPanel');
+
+helpButton.addEventListener("mouseover", function () {
+    var x = document.getElementById("help-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+helpButton.addEventListener("mouseout", function () {
+    var x = document.getElementById("help-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+resetButton.addEventListener("click", function () {
+    window.location.reload();
+    // window.location.href = "";
+    return false;
+})
+
+function check() {
+    console.log('check initiated');
+}
+
+function solve() {
+    console.log('solve initiated');
+}
+
+// Bespoke functions_____________________________________
 var hiddenNums = document.querySelectorAll('.invisible');
 var visibleNums = document.querySelectorAll('.visible');
 var skyBlue = document.querySelectorAll('.skyblue');
@@ -10,15 +51,15 @@ window.addEventListener("load", function () {
         number.innerText = ''
     });
 
-// The 3 functions below remove the background color when the page loads
+    // The 3 functions below remove the background color when the page loads
     skyBlue.forEach(box => {
         box.classList.remove("skyblue")
     });
-    
+
     turtle.forEach(box => {
         box.classList.remove("turquoise")
     });
-    
+
     fruit.forEach(box => {
         box.classList.remove("tomato")
     });
@@ -27,12 +68,12 @@ window.addEventListener("load", function () {
 
 // The below function handles the visible numbers
 visibleNums.forEach(number => {
-number.addEventListener("click", function (evt) {
+    number.addEventListener("click", function (evt) {
         const eventTarget = evt.target;
-        if(eventTarget.dataset.show == "true"){
+        if (eventTarget.dataset.show == "true") {
             eventTarget.innerText = '';
             eventTarget.dataset.show = 'false';
-        } else if(eventTarget.dataset.show == "false"){
+        } else if (eventTarget.dataset.show == "false") {
             eventTarget.dataset.show = 'true';
             eventTarget.innerText = eventTarget.dataset.value;
         }
@@ -43,7 +84,7 @@ number.addEventListener("click", function (evt) {
 hiddenNums.forEach(number => {
     number.addEventListener("click", function (evt) {
         const eventTarget = evt.target;
-        if(eventTarget.dataset.show == "false"){
+        if (eventTarget.dataset.show == "false") {
             eventTarget.innerText = eventTarget.dataset.value;
             eventTarget.dataset.show = true;
         } else {
@@ -57,7 +98,7 @@ hiddenNums.forEach(number => {
 skyBlue.forEach(box => {
     box.addEventListener("click", function (e) {
         const eventTarget = e.target;
-        if(eventTarget.dataset.sky == "off"){
+        if (eventTarget.dataset.sky == "off") {
             box.classList.add('skyblue')
             eventTarget.dataset.sky = "on"
         } else {
@@ -70,7 +111,7 @@ skyBlue.forEach(box => {
 turtle.forEach(box => {
     box.addEventListener("click", function (e) {
         const eventTarget = e.target;
-        if(eventTarget.dataset.turtle == "off"){
+        if (eventTarget.dataset.turtle == "off") {
             box.classList.add('turquoise')
             eventTarget.dataset.turtle = "on"
         } else {
@@ -83,7 +124,7 @@ turtle.forEach(box => {
 fruit.forEach(box => {
     box.addEventListener("click", function (e) {
         const eventTarget = e.target;
-        if(eventTarget.dataset.fruit == "off"){
+        if (eventTarget.dataset.fruit == "off") {
             box.classList.add('tomato')
             eventTarget.dataset.fruit = "on"
         } else {
