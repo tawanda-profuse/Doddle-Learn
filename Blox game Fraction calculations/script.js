4bc2321c193e6bb37ad1c74cbcee7a9c793169e8
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-05 14:00:55 +0200

    Added new files (HTML, CSS, JS)

---
+++ Blox game Fraction calculations/script.js
@@ -0,0 +1,40 @@
+// General_____________________________________________
+var checkButton = document.getElementById('checkButton');
+var solveButton = document.getElementById('solveButton');
+var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var helpButton = document.getElementById('helpButton');
+var startPanel = document.getElementById('startPanel');
+
+helpButton.addEventListener("mouseover", function () {
+    var x = document.getElementById("help-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+helpButton.addEventListener("mouseout", function () {
+    var x = document.getElementById("help-text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+})
+
+resetButton.addEventListener("click", function () {
+    window.location.reload();
+    // window.location.href = "";
+    return false;
+})
+
+function check() {
+    console.log('check initiated');
+}
+
+function solve() {
+    console.log('solve initiated');
+}
+// Bespoke functions_____________________________________
