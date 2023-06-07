6524ab044af7dcd94d3f3302740949c1007eef66
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-07 10:01:43 +0200

    Created new help hover effect

--- Interactive Template/script.js
+++ Interactive Template/script.js
@@ -3,26 +3,7 @@ var checkButton = document.getElementById('checkButton');
 var solveButton = document.getElementById('solveButton');
 var startButton = document.getElementById('startButton');
 var resetButton = document.getElementById('resetButton');
-var helpButton = document.getElementById('helpButton');
 var startPanel = document.getElementById('startPanel');
-
-helpButton.addEventListener("mouseover", function () {
-    var x = document.getElementById("help-text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-})
-
-helpButton.addEventListener("mouseout", function () {
-    var x = document.getElementById("help-text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-})
 
 resetButton.addEventListener("click", function () {
     window.location.reload();
