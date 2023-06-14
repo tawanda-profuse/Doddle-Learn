4c68023a8db019ccb389d2b35c64bd4562dfb660
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-14 09:04:08 +0200

    Created consistent layout

--- 1 Fraction Counter/script.js
+++ 1 Fraction Counter/script.js
@@ -6,24 +6,6 @@ var resetButton = document.getElementById('resetButton');
 var helpButton = document.getElementById('helpButton');
 var startPanel = document.getElementById('startPanel');
 
-helpButton.addEventListener("mouseover", function () {
-    var x = document.getElementById("help-text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-})
-
-helpButton.addEventListener("mouseout", function () {
-    var x = document.getElementById("help-text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-})
-
 resetButton.addEventListener("click", function () {
     window.location.reload();
     // window.location.href = "";
