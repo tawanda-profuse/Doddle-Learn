4dfb858c11359aa3fc3b239de1e3043385701db0
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-15 08:22:07 +0200

    Initialized project using Doddle template

---
+++ Multiply algebraic expressions/script.js
@@ -0,0 +1,26 @@
+// General_____________________________________________
+var checkButton = document.getElementById('checkButton');
+var solveButton = document.getElementById('solveButton');
+var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var startPanel = document.getElementById('startPanel');
+
+resetButton.addEventListener("click", function () {
+    window.location.reload();
+    // window.location.href = "";
+    return false;
+})
+
+function check() {
+    console.log('check initiated');
+}
+
+function solve() {
+    console.log('solve initiated');
+}
+
+startButton.addEventListener("click", function () {
+    startPanel.style.display = 'none';
+    startButton.style.display = 'none';
+})
+// Bespoke functions_____________________________________
