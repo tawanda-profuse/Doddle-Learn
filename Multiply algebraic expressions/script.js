d8f8fcc8697455e5da7b998dcce12884e56b464a
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-15 10:57:16 +0200

    Added page icon

--- Multiply algebraic expressions/script.js
+++ Multiply algebraic expressions/script.js
@@ -1,9 +1,9 @@
 // General_____________________________________________
-var checkButton = document.getElementById('checkButton');
-var solveButton = document.getElementById('solveButton');
-var startButton = document.getElementById('startButton');
-var resetButton = document.getElementById('resetButton');
-var startPanel = document.getElementById('startPanel');
+let checkButton = document.getElementById('checkButton');
+let solveButton = document.getElementById('solveButton');
+let startButton = document.getElementById('startButton');
+let resetButton = document.getElementById('resetButton');
+let startPanel = document.getElementById('startPanel');
 
 resetButton.addEventListener("click", function () {
     window.location.reload();
@@ -19,3 +19,52 @@ function solve() {
     console.log('solve initiated');
 }
 // Bespoke functions_____________________________________
+let hiddenNums = document.querySelectorAll('.invisible');
+
+// The function below hides the numbers when the page loads
+// window.addEventListener("load", function () {
+//     hiddenNums.forEach(number => {
+//         number.innerText = ''
+//     });
+// });
+
+// The function below toggles the visibility of the numbers
+hiddenNums.forEach(number => {
+    number.addEventListener("click", function(e){
+        const targetElement = e.target;
+        if (targetElement.dataset.show == 'false') {
+            targetElement.innerText = '5+5'
+            targetElement.dataset.show = true;
+        } else {
+            targetElement.innerText = '';
+            targetElement.dataset.show = false;
+        }
+    });
+});
+
+let plusMinus = ['-', '', '-', '', '-', '', '-', '', '-', ''];
+let letters = ['a', 'b', ''];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, ''];
+let operators = ['+', '-'];
+
+let addOrSubtract = document.querySelectorAll('.plus-minus');
+let coefficients = document.querySelectorAll('.coefficient');
+let variables = document.querySelectorAll('.variable');
+
+addOrSubtract.forEach(item => {
+    item.textContent = plusMinus[Math.floor(Math.random() * 10)]
+    // console.log("Random operator: " + plusMinus[Math.floor(Math.random() * 10)]);
+})
+
+coefficients.forEach(item => {
+    item.textContent = numbers[Math.floor(Math.random() * 10)]
+    // console.log("Random number: " + numbers[Math.floor(Math.random() * 10)]);
+})
+
+variables.forEach(item => {
+    item.textContent = letters[Math.floor(Math.random() * 3)]
+    // console.log("Random letter: " + letters[Math.floor(Math.random() * 3)]);
+})
+
+
+
