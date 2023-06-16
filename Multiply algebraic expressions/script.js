63788ef2b4c711437bbe468321e84f89a2984213
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-16 08:53:07 +0200

    Removed unnecessary variables

--- Multiply algebraic expressions/script.js
+++ Multiply algebraic expressions/script.js
@@ -20,51 +20,102 @@ function solve() {
 }
 // Bespoke functions_____________________________________
 let hiddenNums = document.querySelectorAll('.invisible');
+let multipliers = document.querySelectorAll('.multiplier');
+let multiplicands = document.querySelectorAll('.multiplicand');
+let tableData = document.querySelectorAll('td:not(td:nth-child(1))');
 
-// The function below hides the numbers when the page loads
-// window.addEventListener("load", function () {
-//     hiddenNums.forEach(number => {
-//         number.innerText = ''
-//     });
-// });
+window.addEventListener("load", function () {
+    tableData.forEach(number => {
+        // Hides the numbers when the page loads
+        number.innerHTML = ''
+    });
 
-// The function below toggles the visibility of the numbers
-hiddenNums.forEach(number => {
-    number.addEventListener("click", function(e){
-        const targetElement = e.target;
-        if (targetElement.dataset.show == 'false') {
-            targetElement.innerText = '5+5'
-            targetElement.dataset.show = true;
-        } else {
-            targetElement.innerText = '';
-            targetElement.dataset.show = false;
-        }
+    var [first, second, third] = [];
+
+    // All cells that are not in the first column or first row:
+    hiddenNums.forEach(number => {
+        // The function below toggles the visibility of the numbers
+        number.addEventListener("click", function (e) {
+            const targetElement = e.target;
+            if (targetElement.dataset.show == 'false') {
+                targetElement.innerHTML = `${}`;
+                // targetElement.innerHTML = `
+                // <span class="plus-minus"></span>
+                // <span class="coefficient"></span>
+                // <span class="variable"></span>
+                // <span class="plus-minus"></span>
+                // <span class="coefficient"></span>
+                // <span class="variable"></span>`;
+                targetElement.dataset.show = true;
+            } else {
+                targetElement.innerHTML = '';
+                targetElement.dataset.show = false;
+            }
+        });
+    });
+
+    // All cells in the first column:
+    multiplicands.forEach(number => {
+        let [operand, coefficient, variable, operator, coefficient2] = [
+            operands[Math.floor(Math.random() * 2)],
+            numbers[Math.floor(Math.random() * 9)],
+            letters[Math.floor(Math.random() * 3)],
+            operators[Math.floor(Math.random() * 2)],
+            numbers[Math.floor(Math.random() * 9)]
+        ];
+        second = coefficient;
+        third = coefficient2;
+        // The function below toggles the visibility of the numbers
+        number.addEventListener("click", function (e) {
+            console.log(coefficient, coefficient2)
+            const targetElement = e.target;
+            if (targetElement.dataset.show == 'false') {
+                targetElement.innerHTML = `${operand}${coefficient}${variable}${operator}${coefficient2}${variable}`;
+                // targetElement.innerHTML = `
+                // <span class="plus-minus">${operand}</span>
+                // <span class="coefficient">${coefficient}</span>
+                // <span class="variable">${variable}</span>
+                // <span class="plus-minus">${operator}</span>
+                // <span class="coefficient">${coefficient2}</span>
+                // <span class="variable">${variable}</span>`;
+                targetElement.dataset.show = true;
+            } else {
+                targetElement.innerHTML = '';
+                targetElement.dataset.show = false;
+            }
+        });
+    });
+
+    // All cells in the first row
+    multipliers.forEach(number => {
+        const [operand, coefficient, variable] = [
+            operands[Math.floor(Math.random() * 2)],
+            numbers[Math.floor(Math.random() * 9)],
+            letters[Math.floor(Math.random() * 3)]
+        ];
+        first = coefficient;
+        // The function below toggles the visibility of the numbers
+        number.addEventListener("click", function (e) {
+            const targetElement = e.target;
+            if (targetElement.dataset.show == 'false') {
+                targetElement.innerHTML = `${operand}${coefficient}${variable}`;
+                // targetElement.innerHTML = `
+                // <span class="plus-minus">${operand}</span>
+                // <span class="coefficient">${coefficient}</span>
+                // <span class="variable">${variable}</span>`;
+                targetElement.dataset.show = true;
+            } else {
+                targetElement.innerHTML = '';
+                targetElement.dataset.show = false;
+            }
+        });
     });
 });
 
-let plusMinus = ['-', '', '-', '', '-', '', '-', '', '-', ''];
+let operands = ['-', ''];
 let letters = ['a', 'b', ''];
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, ''];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let operators = ['+', '-'];
-
-let addOrSubtract = document.querySelectorAll('.plus-minus');
-let coefficients = document.querySelectorAll('.coefficient');
-let variables = document.querySelectorAll('.variable');
-
-addOrSubtract.forEach(item => {
-    item.textContent = plusMinus[Math.floor(Math.random() * 10)]
-    // console.log("Random operator: " + plusMinus[Math.floor(Math.random() * 10)]);
-})
-
-coefficients.forEach(item => {
-    item.textContent = numbers[Math.floor(Math.random() * 10)]
-    // console.log("Random number: " + numbers[Math.floor(Math.random() * 10)]);
-})
-
-variables.forEach(item => {
-    item.textContent = letters[Math.floor(Math.random() * 3)]
-    // console.log("Random letter: " + letters[Math.floor(Math.random() * 3)]);
-})
 
 
 
