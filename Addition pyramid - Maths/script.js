35b998742b45922360eff00def2b54a4b2a2f84a
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-06-28 09:40:44 +0200

    Added HTML, CSS, and JS files

---
+++ Addition pyramid - Maths/script.js
@@ -0,0 +1,21 @@
+// General_____________________________________________
+var checkButton = document.getElementById('checkButton');
+var solveButton = document.getElementById('solveButton');
+var startButton = document.getElementById('startButton');
+var resetButton = document.getElementById('resetButton');
+var startPanel = document.getElementById('startPanel');
+
+resetButton.addEventListener("click", function () {
+    window.location.reload();
+    // window.location.href = "";
+    return false;
+})
+
+function check() {
+    console.log('check initiated');
+}
+
+function solve() {
+    console.log('solve initiated');
+}
+// Bespoke functions_____________________________________
