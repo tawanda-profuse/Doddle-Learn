14dde2ff8efa2ca05d3b3b24eeefce4379267334
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-08-26 01:27:39 +0200

    Changed UI layouts

--- Calculating Gradients - HTML Web Ready/script.js
+++ Calculating Gradients - HTML Web Ready/script.js
@@ -18,324 +18,86 @@ function solve() {
     console.log('solve initiated');
 }
 // Bespoke functions_____________________________________
-var utils = {
-  timestamp: function () {
-    return window.performance && window.performance.now
-      ? window.performance.now()
-      : new Date().getTime();
-  },
-  getCanvasContext: function () {
-    return $(".js-canvas")[0].getContext("2d");
-  },
-  calculateCartesian: function (r, theta) {
-    var x = r * Math.cos(theta);
-    var y = r * Math.sin(theta);
-    return {
-      x: x,
-      y: y,
-    };
-  },
-  calculatePolar: function (x, y) {
-    var r = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
-    var theta = Math.atan2(y, x);
-
-    return {
-      magnitude: r,
-      angle: theta,
-    };
-  },
-};
-
-var $canvas = $(".js-canvas");
-(ctx = utils.getCanvasContext()),
-  (canvasWidth = $canvas.width()),
-  (canvasHeight = $canvas.height());
-
-var space = 20,
-  i,
-  thickSpace = 100;
-
-var offsetX = $canvas.width() / 2;
-var offsetY = $canvas.height() / 2;
-
-var center = {
-  x: offsetX,
-  y: offsetY,
-};
-
-var mouse = {
-  x: 0,
-  y: 0,
-};
+var circlepoints = document.querySelectorAll(".circle-point");
+var angleLines = document.querySelector(".red-angle-lines");
+var diagonalLine = document.querySelector(".diagonal-line");
+var vector = document.querySelector("svg").getBoundingClientRect();
+var area = document.querySelector("svg");
+var text = document.querySelectorAll(".text");
+var redCircles = document.querySelectorAll(".red-circle");
 
 let mouseTracking = {
-  down: false,
-  client: null,
+  circle1: false,
+  circle2: false,
 };
 
-// Function below is executed when the mouse is pressed down
-$canvas.on("mousedown", function (ev) { 
-  mouseTracking = {
-    down: true,
-    client: { x: ev.pageX, y: ev.pageY },
-  };
+let angleCoordinates = {
+  M1: { x: 258, y: 70 },
+  L1: { x: 338, y: 70 },
+  M2: { x: 338, y: 130 },
+  M3: { x: 258, y: 130 },
+  L2: { x: 258, y: 70 },
+};
+
+let diagonalCoordinates = {
+  M: { x: 38, y: 294 },
+  L: { x: 408, y: 13 },
+};
+
+// Event Listeners
+circlepoints[0].addEventListener("mousedown", function () {
+  mouseTracking.circle1 = true;
 });
 
-// Function below is executed when the mouse is dragged
-$canvas.on("mousemove", function (ev) {
-  if (mouseTracking.down) {
-    mouse.x = ev.pageX - $canvas.offset().left;
-    mouse.y = ev.pageY - $canvas.offset().top;
+circlepoints[1].addEventListener("mousedown", function () {
+  mouseTracking.circle2 = true;
+});
+
+document.addEventListener("mousemove", function (ev) {
+  // Left circle
+  if (mouseTracking.circle1 === true) {
+    text[1].textContent = Math.floor(Math.atan2(ev.x, ev.y) * 2.5);
+    text[3].textContent = Math.floor((ev.x / 2) / 100);
+    text[4].textContent = text[2].textContent - text[1].textContent;
+    text[5].textContent = text[2].textContent - text[3].textContent;
+    circlepoints[0].setAttribute("cx", Math.floor(ev.x / 2.6));
+    circlepoints[0].setAttribute("cy", Math.floor(ev.y / 1.8));
+    angleCoordinates.L1.x = Math.floor(vector.height - ev.x / 4);
+    angleCoordinates.M1.y = Math.floor(vector.x - ev.y / 2);
+    angleCoordinates.L1.y = Math.floor(vector.x - ev.y / 2);
+    angleCoordinates.L2.y = Math.floor(vector.x - ev.y / 2);
+    angleLines.setAttribute(
+      "d",
+      `M ${angleCoordinates.M1.x} ${angleCoordinates.M1.y} L ${angleCoordinates.L1.x} ${angleCoordinates.L1.y} M ${angleCoordinates.M2.x} ${angleCoordinates.M2.y} M ${angleCoordinates.M3.x} ${angleCoordinates.M3.y} L ${angleCoordinates.L2.x} ${angleCoordinates.L2.y}`
+    );
+    diagonalCoordinates.M.x = Math.floor(ev.x / 20);
+    diagonalCoordinates.M.y = Math.floor(ev.y);
+    diagonalLine.setAttribute("d", `M ${diagonalCoordinates.M.x} ${diagonalCoordinates.M.y} L ${diagonalCoordinates.L.x} ${diagonalCoordinates.L.y}`);
+  }
+  // Top circle
+  if (mouseTracking.circle2 === true) {
+    text[0].textContent = Math.floor(Math.atan2(ev.x, ev.y) * 4);
+    text[1].textContent = Math.floor(Math.atan2(ev.x, ev.y) * 2.5);
+    text[2].textContent = Math.floor((ev.x / 2.3) / 100);
+    text[4].textContent = text[2].textContent - text[1].textContent;
+    text[5].textContent = text[2].textContent - text[3].textContent;
+    circlepoints[1].setAttribute("cx", Math.floor(ev.x / 2.3));
+    circlepoints[1].setAttribute("cy", Math.floor(ev.y / 1.7));
+    angleCoordinates.M3.y = Math.floor(vector.x - ev.y / 1.6);
+    angleCoordinates.M1.x = Math.floor(vector.height - ev.x / 3);
+    angleCoordinates.M3.x = Math.floor(vector.height - ev.x / 3);
+    angleCoordinates.L2.x = Math.floor(vector.height - ev.x / 3);
+    angleLines.setAttribute(
+      "d",
+      `M ${angleCoordinates.M1.x} ${angleCoordinates.M1.y} L ${angleCoordinates.L1.x} ${angleCoordinates.L1.y} M ${angleCoordinates.M2.x} ${angleCoordinates.M2.y} M ${angleCoordinates.M3.x} ${angleCoordinates.M3.y} L ${angleCoordinates.L2.x} ${angleCoordinates.L2.y}`
+    );
+    diagonalCoordinates.L.x = Math.floor(ev.x / 2);
+    diagonalCoordinates.L.y = Math.floor(ev.y / 1.8);
+    diagonalLine.setAttribute("d", `M ${diagonalCoordinates.M.x} ${diagonalCoordinates.M.y} L ${diagonalCoordinates.L.x} ${diagonalCoordinates.L.y}`);
   }
 });
 
-// Function below is executed when the mouse click ends
-  $canvas.on("mouseup", () => {
-  mouseTracking = {
-    down: false,
-    client: null,
-  };
-});
-
-// Draggable circle:
-var drawItem = function (item) {
-  var x = item.x;
-  var y = item.y;
-
-  // Draggable circle
-  ctx.beginPath();
-
-  ctx.moveTo(x, y);
-  ctx.arc(x, y, 10, 0, 2 * Math.PI, false);
-  ctx.fillStyle = "yellow";
-  ctx.strokeStyle = "#FF0000";
-  ctx.lineWidth = 2;
-  ctx.fill();
-
-  ctx.stroke();
-  // Draggable circle: End
-};
-
-var polar = function (r, theta) {
-  return {
-    magnitude: r,
-    angle: theta,
-  };
-};
-
-// General variables
-var hLength,
-  vLength,
-  dLength,
-  p,
-  midpoint,
-  quartpoint,
-  offsetPos,
-  offsetPosP,
-  offsetmidpointP,
-  angle,
-  arcAngleStart,
-  arcCCW;
-
-var drawMouseLines = function () {
-  // Text color for numbers
-  ctx.beginPath();
-  ctx.fillStyle = "navy";
-  ctx.fill();
-  ctx.stroke();
-
-  // Red lines for angles
-  ctx.beginPath();
-  ctx.strokeStyle = "#FF0000";
-  ctx.lineWidth = 3;
-  ctx.font = "20px Arial";
-
-  hLength = mouse.x - center.x;
-  vLength = center.y - mouse.y;
-  dLength = Math.sqrt(Math.pow(hLength, 2) + Math.pow(vLength, 2));
-
-  // Horizontal line.
-  ctx.moveTo(center.x, center.y);
-  ctx.lineTo(mouse.x, center.y);
-  ctx.fillText(
-    Math.abs(Math.floor(hLength)),
-    mouse.x + (center.x - mouse.x) / 2 - 10,
-    center.y - 5
-  );
-
-  // Vertical line.
-  ctx.moveTo(mouse.x, center.y);
-  ctx.lineTo(mouse.x, mouse.y);
-  ctx.fillText(
-    Math.abs(Math.floor(vLength)),
-    mouse.x + 5,
-    center.y + (mouse.y - center.y) / 2
-  );
-
-  // Diagonal line.
-  ctx.moveTo(center.x, center.y);
-  ctx.lineTo(mouse.x, mouse.y);
-
-  p = utils.calculatePolar(hLength, vLength);
-
-  midpoint = utils.calculateCartesian(p.magnitude / 2, p.angle);
-  quartpoint = utils.calculateCartesian(p.magnitude / 4, p.angle);
-
-  ctx.fillText(
-    dLength.toFixed(2),
-    midpoint.x + offsetX + 10,
-    -midpoint.y + offsetY
-  );
-
-  ctx.moveTo(center.x, center.y);
-
-  angle = Math.atan2(-midpoint.y, midpoint.x);
-
-  // Top right.
-  if (angle <= 0 && angle > -Math.PI / 2) {
-    arcAngleStart = 0;
-    arcCCW = true;
-
-    offsetPos = polar(-5, p.angle - Math.PI / 2);
-  }
-  // Top left.
-  else if (angle <= -Math.PI / 2 && angle >= -Math.PI) {
-    arcCCW = false;
-    if (angle == -Math.PI) {
-      arcAngleStart = -Math.PI;
-    } else {
-      arcAngleStart = Math.PI;
-    }
-
-    offsetPos = polar(5, p.angle - Math.PI / 2);
-  }
-  // Bottom right.
-  else if (angle > 0 && angle < Math.PI / 2) {
-    arcAngleStart = 0;
-    arcCCW = false;
-
-    offsetPos = polar(5, p.angle - Math.PI / 2);
-  }
-  // Bottom left.
-  else if (angle > Math.PI / 2 && angle <= Math.PI) {
-    arcAngleStart = Math.PI;
-    arcCCW = true;
-
-    offsetPos = polar(-5, p.angle - Math.PI / 2);
-  } else {
-    offsetPos = polar(0, 0);
-  }
-
-  offsetPosP = utils.calculateCartesian(offsetPos.magnitude, offsetPos.angle);
-  offsetmidpointP = {
-    x: midpoint.x + offsetPosP.x,
-    y: midpoint.y + offsetPosP.y,
-  };
-
-  ctx.arc(center.x, center.y, p.magnitude / 4, arcAngleStart, angle, arcCCW);
-
-  ctx.stroke();
-};
-
-var mouse = {
-  update: function () {},
-  render: function () {
-    // Draw center;
-    drawMouseLines();
-    drawItem(center); // circle at the center
-
-    ctx.beginPath();
-    ctx.strokeStyle = "navy";
-    ctx.moveTo(offsetPosP.x + offsetX, -offsetPosP.y + offsetY);
-    ctx.lineTo(offsetmidpointP.x + offsetX, -offsetmidpointP.y + offsetY);
-    ctx.stroke();
-
-    drawItem(mouse); // draggable circle at the corner
-  },
-};
-
-var grid = {
-  draw: function () {
-    ctx.beginPath();
-    ctx.strokeStyle = "green";
-    ctx.lineWidth = 1;
-    for (i = space; i < canvasWidth; i += space) {
-      ctx.moveTo(i, 0);
-      ctx.lineTo(i, canvasHeight);
-    }
-    for (i = space; i < canvasHeight; i += space) {
-      ctx.moveTo(0, i);
-      ctx.lineTo(canvasWidth, i);
-    }
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    for (i = thickSpace; i < canvasWidth; i += thickSpace) {
-      ctx.moveTo(i, 0);
-      ctx.lineTo(i, canvasHeight);
-    }
-    for (i = thickSpace; i < canvasHeight; i += thickSpace) {
-      ctx.moveTo(0, i);
-      ctx.lineTo(canvasWidth, i);
-    }
-    ctx.stroke();
-  },
-};
-
-var game = {
-  init: function () {},
-  update: function () {
-    mouse.update();
-  },
-  render: function () {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    grid.draw();
-    mouse.render();
-  },
-};
-
-$(function () {
-  game.init();
-
-  var now,
-    dt = 0,
-    last = utils.timestamp(),
-    slow = 1, // slow motion scaling factor
-    step = 1 / 60,
-    slowStep = slow * step,
-    update = game.update,
-    render = game.render;
-  // fpsmeter = new FPSMeter({ decimals: 0, graph: true, theme: 'dark', left: '5px' });
-
-  function frame() {
-    // fpsmeter.tickStart();
-    now = utils.timestamp();
-    dt = dt + Math.min(1, (now - last) / 1000);
-    while (dt > slowStep) {
-      dt = dt - slowStep;
-      update(step);
-    }
-    render(dt / slow);
-    last = now;
-    // fpsmeter.tick();
-    requestAnimationFrame(frame);
-  }
-
-  requestAnimationFrame(frame);
-});
-
-var videoDemo = document.getElementById("videoDemo");
-var implement = document.getElementById("implement");
-var gradientArea = document.querySelector(".gradient-area");
-var videoArea = document.querySelector('.video-area');
-
-videoDemo.addEventListener("click", function(){
-  videoArea.style.display = 'block';
-  gradientArea.style.display = 'none';
-});
-
-implement.addEventListener("click", function(){
-  videoArea.style.display = 'none';
-  gradientArea.style.display = 'block';
+document.addEventListener("mouseup", function () {
+  mouseTracking.circle1 = false;
+  mouseTracking.circle2 = false;
 });
