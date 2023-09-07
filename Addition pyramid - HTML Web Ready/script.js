868bdc5423f5d4cde9ec268208a068590ef0eb46
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2023-09-07 03:56:56 +0200

    Changed layout to SVG

--- Addition pyramid - HTML Web Ready/script.js
+++ Addition pyramid - HTML Web Ready/script.js
@@ -1,29 +1,29 @@
 // General_____________________________________________
-var checkButton = document.getElementById('checkButton');
-var solveButton = document.getElementById('solveButton');
-var startButton = document.getElementById('startButton');
-var resetButton = document.getElementById('resetButton');
-var startPanel = document.getElementById('startPanel');
+var resetButton = document.getElementById("resetButton");
 
 resetButton.addEventListener("click", function () {
-    window.location.reload();
-    // window.location.href = "";
-    return false;
-})
+  window.location.reload();
+  return false;
+});
 
 function check() {
-    console.log('check initiated');
+  console.log("check initiated");
 }
 
 function solve() {
-    console.log('solve initiated');
+  console.log("solve initiated");
 }
 // Bespoke functions_____________________________________
-let three = document.getElementById("three");
-let four = document.getElementById("four");
-let fourthRow = document.querySelector(".row:nth-child(4)");
-let notelets = document.querySelectorAll(".notelet");
-// Random numbers for multiplication
+var three = document.getElementById("three");
+var threeCircle = document.querySelector(".three");
+var four = document.getElementById("four");
+var fourCircle = document.querySelector(".four");
+var rows = document.querySelectorAll(".row");
+var notelets = document.querySelectorAll(".notelet");
+var reveals = document.querySelectorAll(".reveal");
+var numbers = document.querySelectorAll(".number text");
+
+// Random numbers for addition
 let addend1 = (Math.random() * (20 - 1) + 1).toFixed(0);
 let addend2 = (Math.random() * (20 - 1) + 1).toFixed(0);
 let addend3 = (Math.random() * (20 - 1) + 1).toFixed(0);
@@ -35,126 +35,98 @@ let addend8 = (Math.random() * (20 - 1) + 1).toFixed(0);
 let addend9 = (Math.random() * (20 - 1) + 1).toFixed(0);
 let addend10 = (Math.random() * (20 - 1) + 1).toFixed(0);
 
-// CLicking on the number three
+const numberValues = ([
+  numbers[0].textContent,
+  numbers[1].textContent,
+  numbers[2].textContent,
+  numbers[3].textContent,
+  numbers[4].textContent,
+  numbers[5].textContent,
+  numbers[6].textContent,
+  numbers[7].textContent,
+  numbers[8].textContent,
+  numbers[9].textContent,
+] = [
+  Number(addend7) +
+    Number(addend8) +
+    Number(addend8) +
+    Number(addend9) +
+    Number(addend8) +
+    Number(addend9) +
+    Number(addend9) +
+    Number(addend10),
+  Number(addend7) + Number(addend8) + Number(addend8) + Number(addend9),
+  Number(addend8) + Number(addend9) + Number(addend9) + Number(addend10),
+  Number(addend7) + Number(addend8),
+  Number(addend8) + Number(addend9),
+  Number(addend9) + Number(addend10),
+  addend7,
+  addend8,
+  addend9,
+  addend10,
+]);
+
+// Event listeners:
+window.addEventListener("load", function () {
+  rows[3].style.display = "none";
+});
+
 three.addEventListener("click", function () {
-    fourthRow.style.display = 'none';
-    four.style.borderColor = 'navy'
-    three.style.borderColor = 'red'
-    clear();
+  if (rows[3].style.display === "block") {
+    threeCircle.style.stroke = "red";
+    fourCircle.style.stroke = "navy";
+    rows[3].style.display = "none";
+    // re-render all notelets:
+    notelets.forEach((notelet) => {
+      notelet.style.display = "block";
+    });
+  }
 });
 
-// CLicking on the number four
 four.addEventListener("click", function () {
-    fourthRow.style.display = 'flex';
-    four.style.borderColor = 'red';
-    three.style.borderColor = 'navy';
-    
+  threeCircle.style.stroke = "navy";
+  fourCircle.style.stroke = "red";
+  rows[3].style.display = "block";
+  // re-render all notelets:
+  notelets.forEach((notelet) => {
+    notelet.style.display = "block";
+  });
 });
 
-// Cover the numbers with notelets:
-function clear() {
-    notelets.forEach((item, i) => {
-        if(item.classList.contains("notelet-white")){
-            item.classList.remove("notelet-white");
-            item.textContent = '';
-        }
-    })
-}
-
-notelets[0].addEventListener("click", function () {
-    notelets[0].classList.toggle("notelet-white");
-    if (notelets[0].classList.contains("notelet-white")) {
-        notelets[0].textContent = Number(addend7) + Number(addend8) + Number(addend8) + Number(addend9) + Number(addend8) + Number(addend9) + Number(addend9) + Number(addend10);
-    } else {
-        notelets[0].textContent = ''
-    }
+notelets.forEach((notelet) => {
+  notelet.addEventListener("click", function () {
+    notelet.style.display = "none";
+  });
 });
 
-notelets[1].addEventListener("click", function () {
-    notelets[1].classList.toggle("notelet-white");
-    if (notelets[1].classList.contains("notelet-white")) {
-        notelets[1].textContent = Number(addend7) + Number(addend8) + Number(addend8) + Number(addend9);
-    } else {
-        notelets[1].textContent = ''
-    }
+// Revealing/Hiding the notelets:
+reveals[0].addEventListener("click", function () {
+  notelets[0].style.display = "block";
 });
-
-notelets[2].addEventListener("click", function () {
-    notelets[2].classList.toggle("notelet-white");
-    if (notelets[2].classList.contains("notelet-white")) {
-        notelets[2].textContent = Number(addend8) + Number(addend9) + Number(addend9) + Number(addend10);
-    } else {
-        notelets[2].textContent = ''
-    }
+reveals[1].addEventListener("click", function () {
+  notelets[1].style.display = "block";
 });
-
-notelets[3].addEventListener("click", function () {
-    notelets[3].classList.toggle("notelet-white");
-    if (notelets[3].classList.contains("notelet-white")) {
-        notelets[3].textContent = Number(addend7) + Number(addend8);
-    } else {
-        notelets[3].textContent = ''
-    }
+reveals[2].addEventListener("click", function () {
+  notelets[2].style.display = "block";
 });
-
-notelets[4].addEventListener("click", function () {
-    notelets[4].classList.toggle("notelet-white");
-    if (notelets[4].classList.contains("notelet-white")) {
-        notelets[4].textContent = Number(addend8) + Number(addend9);
-    } else {
-        notelets[4].textContent = ''
-    }
+reveals[3].addEventListener("click", function () {
+  notelets[3].style.display = "block";
 });
-
-notelets[5].addEventListener("click", function () {
-    notelets[5].classList.toggle("notelet-white");
-    if (notelets[5].classList.contains("notelet-white")) {
-        notelets[5].textContent = Number(addend9) + Number(addend10);
-    } else {
-        notelets[5].textContent = ''
-    }
+reveals[4].addEventListener("click", function () {
+  notelets[4].style.display = "block";
 });
-
-notelets[6].addEventListener("click", function () {
-    notelets[6].classList.toggle("notelet-white");
-    if (notelets[6].classList.contains("notelet-white")) {
-        notelets[6].textContent = addend7;
-    } else {
-        notelets[6].textContent = ''
-    }
+reveals[5].addEventListener("click", function () {
+  notelets[5].style.display = "block";
 });
-
-notelets[7].addEventListener("click", function () {
-    notelets[7].classList.toggle("notelet-white");
-    if (notelets[7].classList.contains("notelet-white")) {
-        notelets[7].textContent = addend8;
-    } else {
-        notelets[7].textContent = ''
-    }
+reveals[6].addEventListener("click", function () {
+  notelets[6].style.display = "block";
 });
-
-notelets[8].addEventListener("click", function () {
-    notelets[8].classList.toggle("notelet-white");
-    if (notelets[8].classList.contains("notelet-white")) {
-        notelets[8].textContent = addend9;
-    } else {
-        notelets[8].textContent = ''
-    }
+reveals[7].addEventListener("click", function () {
+  notelets[7].style.display = "block";
 });
-
-notelets[9].addEventListener("click", function () {
-    notelets[9].classList.toggle("notelet-white");
-    if (notelets[9].classList.contains("notelet-white")) {
-        notelets[9].textContent = addend10;
-    } else {
-        notelets[9].textContent = ''
-    }
+reveals[8].addEventListener("click", function () {
+  notelets[8].style.display = "block";
 });
-
-
-
-
-
-
-
-
+reveals[9].addEventListener("click", function () {
+  notelets[9].style.display = "block";
+});
