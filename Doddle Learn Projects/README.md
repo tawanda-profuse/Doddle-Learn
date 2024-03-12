2d81fb6945e9ff411619b2f8e7808f73983c7925
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2024-03-12 11:42:48 +0200

    Added new files

---
+++ Doddle Learn Projects/README.md
@@ -0,0 +1,11 @@
+# Doddle Learn Projects
+
+This single page web application showcases all the projects I was involved in from Doddle Learn. The Doddle Learn platform was discontinued, however I felt it necessary to show the projects I was involved in.
+
+## Project List
+
+1. 24-hour Clock Activity
+2. Addition Pyramid
+3. Addition Square
+4. Area of a Trapezium
+5. Blox Game Fraction Calculations
---
+++ Doddle Learn Projects/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <h1>Doddle Learn Projects</h1>
+      <h4>A List of Projects I contributed to the discontinuted Doddle Learn Platform.</h4>
+      <div className='project-list'>
+          <a target="_blank" href="hello">24 Hour Clock Activity</a>
+          <a target="_blank" href="hello">24 Hour Clock Activity</a>
+          <a target="_blank" href="hello">24 Hour Clock Activity</a>
+          <a target="_blank" href="hello">24 Hour Clock Activity</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
