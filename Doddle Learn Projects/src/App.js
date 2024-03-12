c787dabc880aeab112c3a250e9a7b9a0bd9ed97a
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2024-03-12 12:20:38 +0200

    Added relevant links

--- Doddle Learn Projects/src/App.js
+++ Doddle Learn Projects/src/App.js
@@ -6,10 +6,11 @@ function App() {
   return (
     <div className='wrapper'>
       <h1>Doddle Learn Projects</h1>
-      <h4>A List of Projects I contributed to the discontinuted Doddle Learn Platform.</h4>
+      <h4>A List of Projects <span className='link'>I contributed</span> to the discontinuted Doddle Learn Platform.</h4>
+      <a href="https://andrewtech.onrender.com/#about" target="_blank" rel="noreferrer" className='link'>About Me <i className="fas fa-external-link"></i></a>
       <div className='project-list'>
         {projects.map((project) => (
-          <a target="_blank" href={project.url} rel="noreferrer">{project.title}</a>
+          <a href={project.url} target="_blank" rel="noreferrer">{project.title} <i className="fas fa-external-link"></i></a>
         ))}
       </div>
     </div>
