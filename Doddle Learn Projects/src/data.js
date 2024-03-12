4337b5f5a88f61ce4f20e706da774afc1a7b778c
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2024-03-12 12:01:29 +0200

    Added a new site to the project list

--- Doddle Learn Projects/src/data.js
+++ Doddle Learn Projects/src/data.js
@@ -2,7 +2,11 @@ const projects = [
     {
         title: "German Calculator",
         url: "https://german-calculator.netlify.app/"
-    }
+    },
+    {
+        title: "24 Hour Clock Activity",
+        url: "https://24-hour-clock-activity.netlify.app/"
+    },
 ]
 
 module.exports = projects
