b5e882dfb300b802f46bec4ff5636fd25b7aba13
Tawanda Andrew Msengezi <tawanda.msengezi@doddlelearn.com>
2024-03-12 12:32:48 +0200

    Added alphabetical sorting

--- Doddle Learn Projects/src/App.js
+++ Doddle Learn Projects/src/App.js
@@ -2,14 +2,26 @@ import './App.css';
 import projects from './data';
 
 function App() {
-  console.log(projects);
+  const sortedProjects = projects.slice().sort((a, b) => {
+    // Convert both titles to lowercase for case-insensitive sorting
+    const titleA = a.title.toLowerCase();
+    const titleB = b.title.toLowerCase();
+  
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+    return 0;
+  });
   return (
     <div className='wrapper'>
       <h1>Doddle Learn Projects</h1>
       <h4>A List of Projects <span className='link'>I contributed</span> to the discontinuted Doddle Learn Platform.</h4>
       <a href="https://andrewtech.onrender.com/#about" target="_blank" rel="noreferrer" className='link'>About Me <i className="fas fa-external-link"></i></a>
       <div className='project-list'>
-        {projects.map((project) => (
+        {sortedProjects.map((project) => (
           <a href={project.url} target="_blank" rel="noreferrer">{project.title} <i className="fas fa-external-link"></i></a>
         ))}
       </div>
--- Doddle Learn Projects/src/data.js
+++ Doddle Learn Projects/src/data.js
@@ -7,6 +7,10 @@ const projects = [
         title: "24 Hour Clock Activity",
         url: "https://24-hour-clock-activity.netlify.app/"
     },
+    {
+        title: "Addition Pyramid",
+        url: "https://addition-pyramid.netlify.app/"
+    },
 ]
 
 module.exports = projects
