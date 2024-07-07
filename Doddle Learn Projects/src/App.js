90c3dbebbb5d475ef7b312ecfc0881399348176e
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2024-07-07 15:10:49 +0200

    Latest UI Updates

--- Doddle Learn Projects/src/App.js
+++ Doddle Learn Projects/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import projects from './data';
+import "./App.css";
+import projects from "./data";
 
 function App() {
   const sortedProjects = projects.slice().sort((a, b) => {
     // Convert both titles to lowercase for case-insensitive sorting
     const titleA = a.title.toLowerCase();
     const titleB = b.title.toLowerCase();
-  
+
     if (titleA < titleB) {
       return -1;
     }
@@ -16,29 +16,47 @@ function App() {
     return 0;
   });
 
-  const chooseColor = (category)=>{
+  const chooseColor = (category) => {
     switch (category) {
       case "Math":
-        return "blue"
+        return "blue";
       case "Languages":
-        return "green"
+        return "green";
       case "Biology":
-        return "red"
+        return "red";
       case "Physics":
-        return "hotpink"
+        return "hotpink";
       default:
-        return "gray"
+        return "gray";
     }
-  }
-  
+  };
+
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>Doddle Learn Projects</h1>
-      <h4>A list of projects I contributed to the discontinued Doddle Learn Platform.</h4>
-      <a href="https://tawanda-dev.netlify.app" target="_blank" rel="noreferrer" className='link'>About Me <i className="fas fa-external-link"></i></a>
-      <div className='project-list'>
+      <h4>
+        A list of projects I contributed to the discontinued Doddle Learn
+        Platform.
+      </h4>
+      <a
+        href="https://tawanda-dev.netlify.app"
+        target="_blank"
+        rel="noreferrer"
+        className="link"
+      >
+        About Me <i className="fas fa-external-link"></i>
+      </a>
+      <div className="project-list">
         {sortedProjects.map((project) => (
-          <span><a href={project.url} target="_blank" rel="noreferrer">{project.title} <i className="fas fa-external-link"></i></a> <strong className='category' style={{backgroundColor: chooseColor(project.category)}}>{project.category}</strong></span>
+          <a href={project.url} target="_blank" rel="noreferrer">
+              <span>{project.title} <i className="fas fa-external-link"></i></span>
+            <strong
+              className="category"
+              style={{ backgroundColor: chooseColor(project.category) }}
+            >
+              {project.category}
+            </strong>
+          </a>
         ))}
       </div>
     </div>
